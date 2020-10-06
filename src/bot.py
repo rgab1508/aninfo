4742a4d69c92617a73018869c0b5a68a9eb91e2c
Gabriel Rajendran <rgabriel1508@gmail.com>
2020-10-06 10:05:16 +0530

    working on embed

--- src/bot.py
+++ src/bot.py
@@ -12,7 +12,7 @@ bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 
 def truncate(txt, l, url=None):
-    if l <= len(txt):
+    if l > len(txt):
         return txt
     else:
         s = txt[:l]
@@ -38,32 +38,40 @@ async def search(ctx, *args):
     res = get_media_by_name(name, "ANIME")
     await searching(ctx, name)
     data = res['data']['Media']
-    print(data, type(data), data.keys())
-    title = f'[{data["title"]["english"]}]({data["siteUrl"]})'
-    em = Embed(title=title, color=Color(0xE5E242))
+    if data is None:
+        await ctx.send("Can't Find the anime you are looking for :(")
+        return
+    #print(data, type(data), data.keys())
+    #title = f'[{data["title"]["english"]}]({data["siteUrl"]})'
+    em = Embed(title=data['type'], color=Color.purple())
     em.set_image(url=data['bannerImage'])
-    em.set_author(name=data['type'])
-    des = truncate(data['description'], 350, url=data['siteUrl'])
+    em.set_author(name=data['title']['english'], url=data['siteUrl'])
+    des = truncate(data['description'], 400)
     em.add_field(name="Description:", value=des)
     em.add_field(name="Status:", value=data['status'], inline=True)
     em.add_field(name="Format/Duration:", value=f"{data['format']} / {data['duration']} mins")
-    s_e = "Yet Not Released" if data['startDate']['day'] is None else f"{data['startDate']['day']}/{data['startDate']['month']}/{data['startDate']['year']}"
+    s_e = "Yet Not Released" if data['startDate']['day'] is None else f"{data['startDate']['day']}.{data['startDate']['month']}.{data['startDate']['year']}"
     s_e += " - "
     s_e += "None" if data['startDate']['day'] is None else ""
     s_e += "Ongoing" if data['endDate']['day'] is None else ""
-    s_e += f"{data['endDate']['day']}/{data['endDate']['month']}/{data['endDate']['year']}" if data['endDate']['day'] else ""
-    em.add_field(name="Start/End:", value=s_e)
+    s_e += f"{data['endDate']['day']}.{data['endDate']['month']}.{data['endDate']['year']}" if data['endDate']['day'] else ""
+    em.add_field(name="Date(Start-End):", value=s_e)
     em.add_field(name="Episodes: ", value=data['episodes'])
-    em.add_field(name="Genres:", value=truncate(", ".join(data['genres']), 35))
+    em.add_field(name="Genres:", value=truncate(", ".join(data['genres']), 70))
     em.add_field(name="Average Score:", value=f"{data['averageScore']}%")
-    em.add_field(name="Favourites: ", value=f"{data['favourites']} liked this")
+    em.add_field(name="Favourites: ", value=f"{data['favourites']} people liked this")
     e_plus = "Yes" if data['isAdult'] == "true" else "No"
     em.add_field(name="18+ :", value=e_plus)
     st = []
     for i in data['studios']['nodes']:
         st.append(i['name'])
-    st = " ,".join(st)
+    st = ", ".join(st)
     em.add_field(name="Studios:", value=truncate(st, 50))
+    ot = []
+    for i in data['title'].keys():
+        ot.append(data['title'][i])
+    ot = ", ".join(ot)
+    em.add_field(name="Other Names:", value=ot)
     await ctx.send(embed=em)
 
 @bot.command()
